f0b7ccf7d0029ffbb9933e7f1f5af86819170570
Alexey Misakov <alex.misakov@gmail.com>
2022-04-15 16:40:44 +0300

    product_form_v1, part-1

--- 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
@@ -1,35 +1,45 @@
 import fetchJson from './utils/fetch-json.js';
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
-const CHUNK_LENGTH = 30;
 
 export default class SortableTable {
+   CHUNK_LENGTH = 30;
 
-  _handler = event => {
-    let field = event.target.closest('[class="sortable-table__cell"]');
+  _sortOnClick = event => {
+    let field = event.target.closest('[data-sortable="true"]');
 
-    if (!field || field.dataset.sortable === `false`) return;
+    if (!field) return;
 
     field.dataset.order = field.dataset.order === 'desc' ? 'asc' : 'desc';
+    this.sorted.id = field.dataset.id;
+    this.sorted.order = field.dataset.order;
+
     this.sort(field.dataset.id, field.dataset.order);
-    this.moveArrow(field);
+    this.removeArrowElement();
+    this.addArrowElement(field);
   }
 
   _updateOnScroll = event => {
-    let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
+    const windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
 
-    if (windowRelativeBottom <= document.documentElement.clientHeight) {
+    if (windowRelativeBottom <= document.documentElement.clientHeight && !this.loading) {
+      this.loading = true;
+
       this.update();
+
+      this.loading = false;
     }
   }
 
   constructor(headersConfig, {
     url = '',
+    isSortLocally = false,
     sorted = {
       id: headersConfig.find(item => item.sortable).id,
       order: `asc`,
+      begin : isSortLocally ? null : 0,
+      end : isSortLocally ? null : this.CHUNK_LENGTH,
     },
-    isSortLocally = false,
   } = {}) {
     this.headersConfig = headersConfig;
     this.sorted = sorted;
@@ -43,71 +53,69 @@ export default class SortableTable {
   }
 
   getTemplate() {
-    let container = document.createElement('div');
+    const container = document.createElement('div');
     container.setAttribute('class', 'products-list__container');
     container.setAttribute('data-element', 'productsContainer');
 
-    let table = document.createElement('div');
+    const table = document.createElement('div');
     table.setAttribute('class', 'sortable-table');
-    table.innerHTML = this.getTableHeader() + `<div data-element="body" class="sortable-table__body"></div>`;
+    table.innerHTML = `${this.getTableHeader()}
+      <div data-element="body" class="sortable-table__body"></div>
+      <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+      <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">No products</div>`;
 
-    this.subElements = {
-      header: table.querySelector(`[data-element="header"]`),
-      body: table.querySelector(`[data-element="body"]`),
-      previouslySortedField : null,
-    };
-
+    this.subElements = this.getSubElements(table);
     container.append(table);
     this.element = container;
   }
 
   async render() {
-    this.url.searchParams.set('_sort', this.sorted.id);
-    this.url.searchParams.set('_order', this.sorted.order);
+    this.data =  await this.loadData();
+    this.subElements.body.innerHTML = this.getTableBody(this.data);
+    this.addArrowElement(this.subElements.header.querySelector(`[data-id=${this.sorted.id}]`));
+  }
 
-    if (!this.isSortLocally) {
-      this.url.searchParams.set('_start', this.currentLength);
-      this.url.searchParams.set('_end', CHUNK_LENGTH);
-      this.currentLength += CHUNK_LENGTH;
+  getSubElements(table) {
+    return {
+      header: table.querySelector(`[data-element="header"]`),
+      body: table.querySelector(`[data-element="body"]`),
     }
-
-    this.data = await this.loadData();
-    this.subElements.body.innerHTML = this.getTableRows(this.data);
   }
 
   addEventListeners() {
-    this.subElements.header.addEventListener('pointerdown', this._handler);
+    this.subElements.header.addEventListener('pointerdown', this._sortOnClick);
     if (!this.isSortLocally) {
       window.addEventListener('scroll', this._updateOnScroll);
     }
   }
 
   removeEventListeners() {
-    this.subElements.header.removeEventListener('pointerdown', this._handler);
+    this.subElements.header.removeEventListener('pointerdown', this._sortOnClick);
     if (!this.isSortLocally) {
       window.removeEventListener('scroll', this._updateOnScroll);
     }
   }
 
   getTableHeader() {
-
-    return `<div data-element="header" class="sortable-table__header sortable-table__row">` +
-      this.headersConfig.map(column => {
-        return `<div class="sortable-table__cell" data-id="${column.id}" data-sortable="${column.sortable}" data-order="asc">
-                     <span>${column.title}</span>
-                  </div>`;
-      }).join('') + `</div>`;
+    return `<div data-element="header" class="sortable-table__header sortable-table__row">
+        ${this.getHeaderRow()}</div>`;
   }
 
-  getTableRows(inputData) {
+  getHeaderRow() {
+    return this.headersConfig.map(column => {
+      return `<div class="sortable-table__cell" data-id="${column.id}" data-sortable="${column.sortable}" data-order="asc">
+          <span>${column.title}</span>
+      </div>`;
+    }).join('');
+  }
 
+  getTableBody(inputData) {
     return inputData.map(item => {
       return `<a href="/products/${item.id}" class="sortable-table__row">${this.getTableRow(item)}`;
     }).join('') + `</a>`;
   }
 
   getTableRow(item) {
-
     return this.headersConfig.map(column => {
       return column.id === 'images' || column.id === 'status'?
         column.template(item[column.id]) :
@@ -115,7 +123,7 @@ export default class SortableTable {
     }).join('');
   }
 
-  sort(field, order='asc') {
+  sort(field, order) {
     if (this.isSortLocally) {
       this.sortOnClient(field, order);
     } else {
@@ -127,7 +135,7 @@ export default class SortableTable {
     let index = this.headersConfig.findIndex(obj => obj.id === field);
     let sortedArray = this.sortData(field, index, order);
 
-    this.subElements.body.innerHTML = this.getTableRows(sortedArray);
+    this.subElements.body.innerHTML = this.getTableBody(sortedArray);
   }
 
   sortData(field, index, order) {
@@ -142,50 +150,46 @@ export default class SortableTable {
       if (this.headersConfig[index].sortType === 'string') {
         return direction * a[field].localeCompare(b[field], ['ru', 'en'], {caseFirst: 'upper'});
       }
-      // if (this.headersConfig[index].sortType === 'any_other_sortType') {
-      //   // TO DO - implement sorting by 'any_other_sortType';
-      // }
       return direction * (a[field] - b[field]);
     });
   }
 
-  setURL(field, order) {
-    this.url.searchParams.set('_sort', field);
-    this.url.searchParams.set('_order', order);
-    this.url.searchParams.set('_start', this.currentLength);
-    this.url.searchParams.set('_end', CHUNK_LENGTH);
-  }
-
-  sortOnServer(field, order) {
-    this.currentLength = 0;
-    this.setURL(field, order);
-
-    this.loadData()
-      .then(newData => this.subElements.body.innerHTML = this.getTableRows(newData));
+  async sortOnServer(field, order) {
+    let newData = await this.loadData();
+    this.subElements.body.innerHTML = this.getTableBody(newData);
   }
 
   async loadData() {
+    this.url.searchParams.set('_sort', this.sorted.id);
+    this.url.searchParams.set('_order', this.sorted.order);
+
+    if (this.sorted.begin != null && this.sorted.end != null) {
+      this.url.searchParams.set('_start', this.sorted.begin);
+      this.url.searchParams.set('_end', this.sorted.end);
+    }
+
     return await fetchJson(this.url);
   }
 
   update() {
-    this.url.searchParams.set('_start', this.currentLength);
-    this.url.searchParams.set('_end', String(this.currentLength + CHUNK_LENGTH));
-    this.currentLength += CHUNK_LENGTH;
+    this.sorted.begin = this.sorted.end;
+    this.sorted.end += this.CHUNK_LENGTH;
+
+    this.element.classList.add(`sortable-table_loading`);
 
     this.loadData()
-      .then(newData => this.subElements.body.insertAdjacentHTML('beforeend', this.getTableRows(newData)));
+      .then(newData => this.subElements.body.insertAdjacentHTML('beforeend', this.getTableBody(newData)));
 
+    this.element.classList.remove(`sortable-table_loading`);
   }
 
-  moveArrow(field) {
-    let arrow = `<span data-element="arrow" class="sortable-table__sort-arrow"><span class="sort-arrow"></span></span>`;
+  removeArrowElement() {
+    this.subElements.header.querySelector(`[data-element="arrow"]`).remove();
+  }
 
-    if (this.subElements.previouslySortedField) {
-      this.subElements.previouslySortedField.lastElementChild.remove();
-    }
-    field.insertAdjacentHTML('beforeend', arrow);
-    this.subElements.previouslySortedField = field;
+  addArrowElement(field) {
+    field.insertAdjacentHTML('beforeend',
+      `<span data-element="arrow" class="sortable-table__sort-arrow"><span class="sort-arrow"></span></span>`);
   }
 
   remove() {
--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -5,11 +5,157 @@ const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ProductForm {
+
   constructor (productId) {
     this.productId = productId;
+    this.urlPoducts = new URL('api/rest/products', BACKEND_URL);
+    this.urlCategories = new URL('api/rest/categories', BACKEND_URL);
+
+    this.getTemplate();
+    this.getControlElements();
+    this.imageListContainer = this.element.querySelector(`[data-element="imageListContainer"]`);
+    // this.addEventListeners();
   }
 
   async render () {
+    await this.getProductData();
+    await this.getCategoriesList();
+  }
 
+  getTemplate() {
+    let element = document.createElement('div');
+    element.classList.add('product-form');
+    element.innerHTML = `<form data-element="productForm" class="form-grid">
+      <div class="form-group form-group__half_left">
+        <fieldset>
+          <label class="form-label">Название товара</label>
+          <input required="" type="text" name="title" class="form-control" placeholder="Название товара">
+        </fieldset>
+      </div>
+      <div class="form-group form-group__wide">
+        <label class="form-label">Описание</label>
+        <textarea required="" class="form-control" name="description" data-element="productDescription" placeholder="Описание товара"></textarea>
+      </div>
+      <div class="form-group form-group__wide" data-element="sortable-list-container">
+        <label class="form-label">Фото</label>
+        <div data-element="imageListContainer"></div>
+        <button type="button" name="uploadImage" class="button-primary-outline"><span>Загрузить</span></button>
+      </div>
+      <div class="form-group form-group__half_left">
+        <label class="form-label">Категория</label>
+        <select class="form-control" name="subcategory">
+        </select>
+      </div>
+      <div class="form-group form-group__half_left form-group__two-col">
+        <fieldset>
+          <label class="form-label">Цена ($)</label>
+          <input required="" type="number" name="price" class="form-control" placeholder="100">
+        </fieldset>
+        <fieldset>
+          <label class="form-label">Скидка ($)</label>
+          <input required="" type="number" name="discount" class="form-control" placeholder="0">
+        </fieldset>
+      </div>
+      <div class="form-group form-group__part-half">
+        <label class="form-label">Количество</label>
+        <input required="" type="number" class="form-control" name="quantity" placeholder="1">
+      </div>
+      <div class="form-group form-group__part-half">
+        <label class="form-label">Статус</label>
+        <select class="form-control" name="status">
+          <option value="1">Активен</option>
+          <option value="0">Неактивен</option>
+        </select>
+      </div>
+      <div class="form-buttons">
+        <button type="submit" name="save" class="button-primary-outline">
+          Сохранить товар
+        </button>
+      </div>
+    </form>`;
+
+    this.element = element;
+  }
+
+  getControlElements() {
+    this.controlElements = Array.from(this.element.querySelectorAll(`.form-control`))
+      .reduce((previous, current) => {
+        previous[current.name] = current;
+        return previous;
+      }, {});
+  }
+
+  async loadProductData() {
+    this.urlPoducts.searchParams.set('id', this.productId);
+
+    return await fetchJson(this.urlPoducts);
+  }
+
+  async getProductData() {
+    const data = await this.loadProductData();
+
+    if (!data) return;
+
+    for (const [key, val] of Object.entries(this.controlElements)) {
+      val.value = data[0][key];
+    }
+
+    this.imageListContainer.innerHTML = this.getImageList(data[0].images);
+  }
+
+  async loadCategoriesData() {
+    this.urlCategories.searchParams.set('_sort', `weight`);
+    this.urlCategories.searchParams.set('_refs', `subcategory`);
+
+    const data = await fetchJson(this.urlCategories);
+    const categoriesData = [];
+
+    data.forEach( category => {
+      categoriesData.push(...category.subcategories.map( sub => {
+        return { value: sub.id, content: `${category.title} &gt; ${sub.title}` };
+      }));
+    });
+
+    return categoriesData;
+  }
+
+  async getCategoriesList() {
+    const categoriesData = await this.loadCategoriesData();
+
+    this.controlElements.subcategory.innerHTML = categoriesData.map( item => {
+      return `<option value="${item.value}">${item.content}</option>`;
+    }).join('');
+  }
+
+  getImageList(images) {
+    const imageList = images.map(image => this.getImageListItem(image)).join('');
+    return `<ul class="sortable-list">${imageList}</ul>`;
+  }
+
+  getImageListItem(item) {
+    return `
+      <li class="products-edit__imagelist-item sortable-list__item" style="">
+        <input type="hidden" name="url" value="${item.url}">
+        <input type="hidden" name="source" value="${item.source}">
+        <span>
+          <img src="icon-grab.svg" data-grab-handle="" alt="grab">
+          <img class="sortable-table__cell-img" alt="Image" src="${item.url}">
+          <span>${item.source}</span>
+        </span>
+        <button type="button">
+          <img src="icon-trash.svg" data-delete-handle="" alt="delete">
+        </button></li>`;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    // this.removeEventListeners();
+    this.remove();
+    this.element = null;
   }
 }
